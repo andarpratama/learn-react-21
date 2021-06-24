7e923f0ccb4ed23c472cc86d9b7ea2253dc2cd27
andarpratama <master.amarta@gmail.com>
2021-06-25 06:51:11 +0700

    fixing footer

--- demo-stylesheet/src/components/Footer.js
+++ demo-stylesheet/src/components/Footer.js
@@ -1,9 +1,9 @@
 const Footer = () => {
    return (
-      <footer class="border-top py-4 mt-4">
-         <div class="container d-flex justify-content-between">
+      <footer className="border-top py-4 mt-4">
+         <div className="container d-flex justify-content-between">
             <span>© 2021 saltTalk | All rights reserved</span>
-            <span>Created by <a href="#" class="text-decoration-none text-dark underline-text"> <i class="fab fa-instagram"></i> @andhar.prathama</a></span>
+            <span>Created by <a href="/" className="text-decoration-none text-dark underline-text"> <i className="fab fa-instagram"></i> @andhar.prathama</a></span>
          </div>
       </footer>
    )
